44470383262cbd0fc969b76549c84764688d7e40
pedrojoel705 <pedrojoel705@gmail.com>
2022-07-24 18:45:57 -0300

    I add a module that allows you to optimize the ajax requests you need

--- app/App.js
+++ app/App.js
@@ -1,9 +1,24 @@
 import api from './helpers/wp_api.js';
+import { ajax } from './helpers/ajax.js';
 
 export function App() {
   document.getElementById(
     'root'
   ).innerHTML = `<h1>Bienvenidos a mi primer SPA con Vanilla JS</h1>`;
 
-  console.log(api);
+  ajax({
+    url: api.POST,
+    cbSuccess: (post) => {
+      console.log(post);
+    },
+  });
+
+  ajax({
+    url: api.CATEGORIES,
+    cbSuccess: (categories) => {
+      console.log(categories);
+    },
+  });
+
+  console.log();
 }
